c89147d8690e2d9dc53126e909d8323234c48ea5
Prakhar Rai <53911572+PrakharRai07@users.noreply.github.com>
2022-07-22 17:41:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1,randomNumber2;
+
+randomNumber1 = Math.random();
+randomNumber1 = randomNumber1*6;
+randomNumber1 = Math.floor(randomNumber1);
+randomNumber1++;
+
+randomNumber2 = Math.random();
+randomNumber2 = randomNumber2*6;
+randomNumber2 = Math.floor(randomNumber2);
+randomNumber2++;
+
+
+
+document.querySelector("img").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector("img.img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!!!";
+}
+
+console.log(randomNumber1);
